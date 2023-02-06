52de634fc0938ba050ae688b0cb5ef1ec3f341af
pnzrr <pnzrrr@pm.me>
2023-02-05 18:42:11 -0700

    button hover color

--- src/components/elements/forms/buttons/button.tsx
+++ src/components/elements/forms/buttons/button.tsx
@@ -15,7 +15,7 @@ const Button: FC<ButtonProps> = ({ children, isBlackButton }, ...args) => {
       className={cs(
         "inline-flex items-center rounded border border-neutral-200 bg-neutral-50 px-4 py-2 text-sm font-medium text-p2blue-900  hover:bg-neutral-100 focus:outline-none focus:ring-1 focus:ring-neutral-50 focus:ring-offset-1",
         {
-          "bg-p2gray-900 text-white": isBlackButton,
+          "bg-p2gray-900 text-white hover:bg-zinc-900": isBlackButton,
         }
       )}
       {...args}
