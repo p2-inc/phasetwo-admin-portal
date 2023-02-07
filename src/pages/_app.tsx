2e3b88d6c37749e8c0c8fa32d1e92bce85fc0a45
Garth <244253+xgp@users.noreply.github.com>
2023-02-07 09:20:38 +0100

    testing

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,11 @@ import Layout from "@/components/layouts/layout";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 
+import { KeycloakClient, KeycloakService } from '../services/keycloak.service';
+import { KeycloakContext } from '../services/KeycloakContext';
+import { AccountServiceClient } from '../services/account.service';
+import { AccountServiceContext } from '../services/AccountServiceContext';
+
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <Layout>
@@ -9,3 +14,25 @@ export default function App({ Component, pageProps }: AppProps) {
     </Layout>
   );
 }
+
+/*
+import Keycloak, { KeycloakLoginOptions } from "keycloak-js";
+
+//declare const keycloak: KeycloakClient;
+const keycloak = new Keycloak('keycloak.json');
+
+export default function App({ Component, pageProps }: AppProps) {
+  const keycloakService = new KeycloakService(keycloak);
+  return (
+    <KeycloakContext.Provider value={keycloakService}>
+      <AccountServiceContext.Provider value={new AccountServiceClient(keycloakService)}>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </AccountServiceContext.Provider>
+    </KeycloakContext.Provider>
+  );
+}
+
+
+*/
---
+++ src/services/Features.ts
@@ -0,0 +1,13 @@
+export interface Features {
+    isRegistrationEmailAsUsername: boolean;
+    isEditUserNameAllowed: boolean;
+    isInternationalizationEnabled: boolean;
+    isLinkedAccountsEnabled: boolean;
+    isEventsEnabled: boolean;
+    isMyResourcesEnabled: boolean;
+    isTotpConfigured: boolean;
+    deleteAccountAllowed: boolean;
+    updateEmailFeatureEnabled: boolean;
+    updateEmailActionEnabled: boolean;
+    isViewGroupsEnabled: boolean;
+ }
