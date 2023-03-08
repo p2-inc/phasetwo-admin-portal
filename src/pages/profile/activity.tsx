e2a1e5d569c05c9ed40fd667c239276e09430784
pnzrr <pnzrrr@pm.me>
2023-03-07 20:37:23 -0700

    layout cleanup

--- src/pages/profile/activity.tsx
+++ src/pages/profile/activity.tsx
@@ -49,22 +49,29 @@ const ActivityProfile = () => {
     else return TimeUtil.format(time * 1000);
   };
 
-  const elementId = (item: string, session: SessionRepresentation, element: string='session'): string => {
-    return `${element}-${session.id?.substring(0,7)}-${item}`;
+  const elementId = (
+    item: string,
+    session: SessionRepresentation,
+    element: string = "session"
+  ): string => {
+    return `${element}-${session.id?.substring(0, 7)}-${item}`;
   };
 
-  const findDeviceTypeIcon = (session: SessionRepresentation, device: DeviceRepresentation): React.ReactNode => {
+  const findDeviceTypeIcon = (
+    session: SessionRepresentation,
+    device: DeviceRepresentation
+  ): React.ReactNode => {
     const deviceType: boolean = device.mobile ?? false;
     if (deviceType === true) {
       //return (<MobileIcon className="fill-p2gray-800" id={elementId('icon-mobile', session, 'device')} />);
-      return (<ComputerIcon className="fill-p2gray-800" />);
+      return <ComputerIcon className="fill-p2gray-800" />;
     }
     //return (<ComputerIcon className="fill-p2gray-800" id={elementId('icon-desktop', session, 'device')} />);
-    return (<ComputerIcon className="fill-p2gray-800" />);
+    return <ComputerIcon className="fill-p2gray-800" />;
   };
 
   const findOS = (device: DeviceRepresentation): string => {
-    if (device.os?.toLowerCase().includes('unknown')) {
+    if (device.os?.toLowerCase().includes("unknown")) {
       //return Msg.localize('unknownOperatingSystem');
       return "Unknown";
     }
@@ -72,24 +79,28 @@ const ActivityProfile = () => {
   };
 
   const findOSVersion = (device: DeviceRepresentation): string => {
-    if (device.osVersion?.toLowerCase().includes('unknown')) return '';
+    if (device.osVersion?.toLowerCase().includes("unknown")) return "";
 
     return device.osVersion ?? "";
   };
 
   const makeClientsString = (clients: ClientRepresentation[]): string => {
     let clientsString = "";
-    clients.forEach( (client: ClientRepresentation, index: number) => {
+    clients.forEach((client: ClientRepresentation, index: number) => {
       let clientName: string;
-      if (client.hasOwnProperty('clientName') && (client.clientName !== undefined) && (client.clientName !== '')) {
+      if (
+        client.hasOwnProperty("clientName") &&
+        client.clientName !== undefined &&
+        client.clientName !== ""
+      ) {
         //clientName = Msg.localize(client.clientName);
         clientName = client.clientName;
       } else {
         clientName = client.clientId ?? "";
       }
       clientsString += clientName;
-      if (clients.length > index + 1) clientsString += ', ';
-    })
+      if (clients.length > index + 1) clientsString += ", ";
+    });
     return clientsString;
   };
 
@@ -100,7 +111,6 @@ const ActivityProfile = () => {
     return false;
   };
 
-
   return (
     <div>
       <div className="mb-12">
@@ -109,9 +119,15 @@ const ActivityProfile = () => {
           description="Sign out of any unfamiliar devices."
         />
       </div>
-      { isShowSignOutAll(devices) &&
-      <ConfirmationModal buttonTitle='Sign out all devices' buttonId='sign-out-all' modalTitle='Sign out all devices' modalMessage='This action will sign out all the devices that have signed in to your account, including the current device you are using.' onContinue={() => signOutAll()}/>
-      }
+      {isShowSignOutAll(devices) && (
+        <ConfirmationModal
+          buttonTitle="Sign out all devices"
+          buttonId="sign-out-all"
+          modalTitle="Sign out all devices"
+          modalMessage="This action will sign out all the devices that have signed in to your account, including the current device you are using."
+          onContinue={() => signOutAll()}
+        />
+      )}
       <div className="w-full rounded border border-gray-200 bg-gray-50">
         {isFetching && <ActivityLoader />}
         {!isFetching &&
@@ -124,17 +140,37 @@ const ActivityProfile = () => {
                   >
                     <div className="md:flex md:items-center">
                       <div className="p-4 pr-0 pb-0">
-                      {findDeviceTypeIcon(session, device)}
+                        {findDeviceTypeIcon(session, device)}
                       </div>
                       <div className="p-4 pb-0 text-sm font-semibold text-p2gray-900 md:pl-2">
-                        <span id={elementId('browser', session)} className="pf-u-mr-md session-title">{findOS(device)} {findOSVersion(device)} / {session.browser}</span>
-                        {session.current &&
-                        <><label id={elementId('current-badge', session)} className="block text-sm font-medium text-green-700">Current session</label></>}
+                        <span
+                          id={elementId("browser", session)}
+                          className="pf-u-mr-md session-title"
+                        >
+                          {findOS(device)} {findOSVersion(device)} /{" "}
+                          {session.browser}
+                        </span>
+                        {session.current && (
+                          <>
+                            <label
+                              id={elementId("current-badge", session)}
+                              className="block text-sm font-medium text-green-700"
+                            >
+                              Current session
+                            </label>
+                          </>
+                        )}
                       </div>
                       <div>
-                      {!session.current &&
-                      <ConfirmationModal buttonTitle='Sign out' buttonId={elementId('sign-out', session)} modalTitle='Sign out' modalMessage='Sign out this session?' onContinue={() => signOutSession(device, session)}/>
-                      }
+                        {!session.current && (
+                          <ConfirmationModal
+                            buttonTitle="Sign out"
+                            buttonId={elementId("sign-out", session)}
+                            modalTitle="Sign out"
+                            modalMessage="Sign out this session?"
+                            onContinue={() => signOutSession(device, session)}
+                          />
+                        )}
                       </div>
                     </div>
                     <div className="md:grid md:grid-cols-5">
@@ -150,7 +186,8 @@ const ActivityProfile = () => {
                       </div>
                       <div className="space-y-1 p-4">
                         <ActivityItem title="Clients">
-                          {session.clients && makeClientsString(session.clients)}
+                          {session.clients &&
+                            makeClientsString(session.clients)}
                         </ActivityItem>
                       </div>
                       <div className="space-y-1 p-4">
