431711ef9fbc61b21cbb1c80580c0887db212d43
pnzrr <pnzrrr@pm.me>
2023-03-07 20:40:16 -0700

    args format

--- src/components/elements/confirmation-modal.tsx
+++ src/components/elements/confirmation-modal.tsx
@@ -1,51 +1,72 @@
 import { FC, useState } from "react";
-import { Dialog } from '@headlessui/react';
+import { Dialog } from "@headlessui/react";
 import Button from "components/elements/forms/buttons/button";
 
 type ConfirmationModalProps = {
-    children?: React.ReactNode;
-    buttonTitle?: string;
-    buttonId?: string;
-    render?(toggle: () => void): React.ReactNode;
-    modalTitle: string;
-    modalMessage?: string;
-    modalContinueButtonLabel?: string;
-    modalCancelButtonLabel?: string;
-    onContinue: () => void;
-    onClose?: () => void;
-    isDisabled?: boolean;
+  children?: React.ReactNode;
+  buttonTitle?: string;
+  buttonId?: string;
+  render?(toggle: () => void): React.ReactNode;
+  modalTitle: string;
+  modalMessage?: string;
+  modalContinueButtonLabel?: string;
+  modalCancelButtonLabel?: string;
+  onContinue: () => void;
+  onClose?: () => void;
+  isDisabled?: boolean;
 };
 
-const ConfirmationModal: FC<ConfirmationModalProps> = (props) => {
-  let [isOpen, setIsOpen] = useState(false)
+const ConfirmationModal: FC<ConfirmationModalProps> = ({
+  children,
+  buttonTitle,
+  buttonId,
+  render,
+  modalTitle,
+  modalMessage,
+  modalCancelButtonLabel,
+  modalContinueButtonLabel,
+  onContinue,
+  onClose,
+  isDisabled,
+}) => {
+  let [isOpen, setIsOpen] = useState(false);
 
   const handleModalToggle = () => {
     setIsOpen(!isOpen);
-    if (props.onClose) props.onClose();
+    if (onClose) onClose();
   };
 
   const handleContinue = () => {
     handleModalToggle();
-    props.onContinue();
+    onContinue();
   };
 
   return (
     <>
-    {!props.render &&
-    <Button id={props.buttonId} onClick={handleModalToggle} disabled={props.isDisabled}>
-        {props.buttonTitle}
-    </Button>}
-    {props.render && props.render(handleModalToggle)}
-    <Dialog open={isOpen} onClose={() => setIsOpen(false)} className="relative z-50">
+      {!render && (
+        <Button id={buttonId} onClick={handleModalToggle} disabled={isDisabled}>
+          {buttonTitle}
+        </Button>
+      )}
+      {render && render(handleModalToggle)}
+      <Dialog
+        open={isOpen}
+        onClose={() => setIsOpen(false)}
+        className="relative z-50"
+      >
         <Dialog.Panel>
-            <Dialog.Title>{props.modalTitle}</Dialog.Title>
-            <Dialog.Description>{props.modalMessage}</Dialog.Description>
-            {props.children}
-            <Button onClick={() => handleContinue()}>{props.modalContinueButtonLabel}</Button>
-            <Button onClick={() => handleModalToggle()}>{props.modalCancelButtonLabel}</Button>
+          <Dialog.Title>{modalTitle}</Dialog.Title>
+          <Dialog.Description>{modalMessage}</Dialog.Description>
+          {children}
+          <Button onClick={() => handleContinue()}>
+            {modalContinueButtonLabel}
+          </Button>
+          <Button onClick={() => handleModalToggle()}>
+            {modalCancelButtonLabel}
+          </Button>
         </Dialog.Panel>
-    </Dialog>
-  </>
+      </Dialog>
+    </>
   );
 };
 
