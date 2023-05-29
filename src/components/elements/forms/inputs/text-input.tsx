1a686a9407c77bf4fc35d6ff2dd1c0f7c2b4b687
pnzrr <pnzrrr@pm.me>
2023-05-29 10:32:34 -0600

    remove unused components

--- src/components/elements/forms/inputs/text-input.tsx
+++ src/components/elements/forms/inputs/text-input.tsx
@@ -1,16 +1,2 @@
 export const BasicFormClasses =
   "block rounded border-gray-300 dark:border-zinc-600 dark:text-zinc-200 sm:text-sm bg-white dark:bg-p2dark-1000 disabled:opacity-50 disabled:cursor-not-allowed hover:border-gray-500 transition";
-
-const FormTextInput = () => {
-  return (
-    <input
-      type="text"
-      name="text"
-      id="text"
-      className={BasicFormClasses}
-      placeholder="placeholder"
-    />
-  );
-};
-
-export default FormTextInput;
--- src/pages/profile/general.tsx
+++ src/pages/profile/general.tsx
@@ -6,23 +6,6 @@ import Internationalization from "./components/internationalization";
 const GeneralProfile = () => {
   const { features: featureFlags } = config.env;
 
-  // Preserving for later use
-  // {
-  //   (data?.userProfileMetadata?.attributes || [])
-  //     .filter((attribute) => !isRootAttribute(attribute.name))
-  //     .map((attribute) => (
-  //       <FormTextInputWithLabel
-  //         slug={attribute.name!}
-  //         label={
-  //           (isBundleKey(attribute.displayName)
-  //             ? t(unWrap(attribute.displayName!))
-  //             : attribute.displayName) || attribute.name!
-  //         }
-  //         inputArgs={{ value: data ? data[attribute.name!] : "" }}
-  //       />
-  //     ));
-  // }
-
   return (
     <div>
       <ProfileData />
